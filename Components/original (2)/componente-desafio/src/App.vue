b7e57e70a7890d388abb6d3cf96bda69a92b8ff8
nome-do-seu-usuario-de-login-do-github <pedrovinicius35@hotmail.com>
2020-03-09 15:24:05 -0300

    Atualização - componentes

---
+++ Components/original (2)/componente-desafio/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<v-app>
+		<v-menu/>
+
+		<app-toolbar/>
+
+		<v-content>
+			<v-carousel></v-carousel>
+		</v-content>
+
+	<v-footer/>
+	</v-app>
+</template>
+
+<script>
+import Footer from './Footer'
+import Carousel from './Carousel'
+import Menu from './Menu'
+import Toolbar from './Toolbar'
+
+export default {
+	components: { 'v-footer': Footer, 'v-carousel': Carousel, 'v-menu': Menu, 'app-toolbar': Toolbar},
+	data() {
+		return {
+			itensMenu: [
+				{titulo: 'Início', icon: 'dashboard'},
+				{titulo: 'Sobre', icon: 'question_answer'}
+			],
+			fotos: [
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
+			]
+		}
+	}
+}
+</script>
---
+++ Components/original (2)/componente-desafio/src/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer class="pa-3" app>
+    <v-spacer></v-spacer>
+    <div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
+<style lang="stylus" scoped></style>
